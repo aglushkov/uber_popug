442da0bf7783be1c653908fca3214ee134461899
Andrey Glushkov <aglushkov@shakuro.com>
2022-05-14 17:16:39 +0300

    Add event-schema-registry

--- _authn/setup.rb
+++ _authn/setup.rb
@@ -1,57 +1,17 @@
-require "bundler/inline"
+# frozen_string_literal: true
 
-gemfile do
-  source "https://rubygems.org"
-  ruby "3.1.0"
+require "bundler/setup"
+Bundler.require(:default, ENV["RACK_ENV"] || "development")
 
-  gem "roda"
-  gem "puma"
-  gem "pg"
-  gem "bunny"
-  gem "dry-schema"
-  gem "sequel"
-  gem "oj"
-  gem "bcrypt"
-  gem "dotenv"
-  gem "debug"
-end
-
-require "roda"
-require "puma"
-require "pg"
-require "dry-schema"
-require "sequel"
-require "oj"
-require "bcrypt"
-require "dotenv"
-require "debug"
 require "securerandom"
 
 # Config
-require "./config/bcrypt"
-require "./config/dotenv"
-require "./config/dry-schema"
-require "./config/sequel"
-require "./config/bunny"
+Dir["./config/*.rb"].each { |file| require file }
 
-# Forms
-require "./app/forms/register_form"
-require "./app/forms/login_form"
-require "./app/forms/authenticate_form"
+# App files
+Dir["./app/**/*.rb"].each { |file| require file }
 
-# Models
-require "./app/models/account"
-
-# Serializers
-require "./app/serializers/account"
-require "./app/serializers/event_account_created"
-require "./app/serializers/session"
-
-# Services
-require "./app/services/validation"
-require "./app/services/publish_event"
-
-# App
+# Roda App
 require "./app"
 
 App.freeze
